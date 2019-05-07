d3a3b3f391d858e2b8f59850c96a57656f5fdcf8
Mauro Daprotis <mauro.daprotis@globant.com>
2019-05-07 12:08:20 -0300

    Add logout resolver

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import 'reflect-metadata';
-import { ApolloServer } from 'apollo-server-express';
-import Express from 'express';
-import { buildSchema } from 'type-graphql';
-import { createConnection } from 'typeorm';
-import session from 'express-session';
-import connectRedis from 'connect-redis';
-import cors from 'cors';
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import Express from "express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
 
-import { redis } from './redis';
+import { redis } from "./redis";
 
 const main = async () => {
   await createConnection();
@@ -17,12 +17,12 @@ const main = async () => {
       if (context.req.session.userId) return true;
 
       return false;
-    },
+    }
   });
 
   const apolloServer = new ApolloServer({
     schema,
-    context: ({ req }: any) => ({ req }),
+    context: ({ req, res }: any) => ({ req, res })
   });
 
   const app = Express();
@@ -32,31 +32,31 @@ const main = async () => {
   app.use(
     cors({
       credentials: true,
-      origin: 'http://localhost:3000',
+      origin: "http://localhost:3000"
     })
   );
 
   app.use(
     session({
       store: new RedisStore({
-        client: redis as any,
+        client: redis as any
       }),
-      name: 'qid',
-      secret: 'THIS_SOULD_BE_AN_ENV_VARIABLE',
+      name: "qid",
+      secret: "THIS_SOULD_BE_AN_ENV_VARIABLE",
       resave: false,
       saveUninitialized: true,
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-      },
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7
+      }
     })
   );
 
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
-    console.log('Server started on http://localhost:4000/graphql');
+    console.log("Server started on http://localhost:4000/graphql");
   });
 };
 
--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,10 +1,10 @@
-import { Length, IsEmail } from 'class-validator';
-import { InputType, Field } from 'type-graphql';
-import { IsEmailAlreadyExists } from './IsEmailAlreadyExists';
-import { PasswordInput } from '../../shared/PasswordInput';
+import { Length, IsEmail } from "class-validator";
+import { InputType, Field } from "type-graphql";
+import { IsEmailAlreadyExists } from "./IsEmailAlreadyExists";
+import { PasswordMixin } from "../../shared/PasswordMixin";
 
 @InputType()
-export class RegisterInput extends PasswordInput {
+export class RegisterInput extends PasswordMixin(class {}) {
   @Field()
   @Length(1, 255)
   firstName: string;
@@ -16,7 +16,7 @@ export class RegisterInput extends PasswordInput {
   @Field()
   @IsEmail()
   @IsEmailAlreadyExists({
-    message: 'Email already in use',
+    message: "Email already in use"
   })
   email: string;
 }
